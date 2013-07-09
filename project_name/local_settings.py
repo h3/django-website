bb04a482a0880c114c4d676f907bf3ee839fe892
Maxime Haineault <max@motion-m.ca>
2013-07-09 00:23:47 -0400

    Update local_settings.py
    Updated default local settings

--- project_name/local_settings.py
+++ project_name/local_settings.py
@@ -23,6 +23,25 @@ if DEV_SERVER:
 
 
 if DEBUG_TOOLBAR:
-    INTERNAL_IPS = ('127.0.0.1', '0.0.0.0', 'dev')
+    INTERNAL_IPS = ('127.0.0.1', '0.0.0.0', 'dev', 'localhost')
     INSTALLED_APPS = INSTALLED_APPS + ('debug_toolbar',)
     MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+    #HIDE_DJANGO_SQL = True
+    DEBUG_TOOLBAR_CONFIG = {
+        'INTERCEPT_REDIRECTS': False,
+        #'EXTRA_SIGNALS': ['myproject.signals.MySignal'],
+        #'HIDE_DJANGO_SQL': True,
+        #'ENABLE_STACKTRACES' : True,
+    }
+    DEBUG_TOOLBAR_PANELS = (
+        'debug_toolbar.panels.version.VersionDebugPanel',
+        'debug_toolbar.panels.timer.TimerDebugPanel',
+        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+        'debug_toolbar.panels.headers.HeaderDebugPanel',
+        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+        'debug_toolbar.panels.template.TemplateDebugPanel',
+        'debug_toolbar.panels.sql.SQLDebugPanel',
+        'debug_toolbar.panels.signals.SignalDebugPanel',
+        'debug_toolbar.panels.logger.LoggingPanel',
+    )
+
